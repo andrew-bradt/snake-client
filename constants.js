99faafdc21630499eec1c1fddfb5ffd3c44e5ff0
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-16 14:25:47 -0500

    Moved all key constants to constants.js, refactored handleUserInput to lookup values from imported objects

--- constants.js
+++ constants.js
@@ -1,7 +1,23 @@
 const IP = '192.168.0.8';
 const PORT = '50541';
+const KEY_MAPPINGS = {
+  EXIT: '\u0003',
+  MOVEMENT: {
+    w: 'up',
+    a: 'left',
+    s: 'down',
+    d: 'right'
+  },
+  CANNED_MESSAGES: {
+    p: 'ssssssss',
+    o: 'sSssSsSs',
+    i: 'sSSSSS??',
+    l: 'SSSSS!!!'
+  }
+};
 
 module.exports = {
   IP,
-  PORT
+  PORT,
+  KEY_MAPPINGS
 };
--- input.js
+++ input.js
@@ -1,36 +1,11 @@
 // Stores the active TCP connection object
+const { EXIT, MOVEMENT, CANNED_MESSAGES } = require('./constants').KEY_MAPPINGS;
 let connection;
 
 const handleUserInput = (key) => {
-  if (key === '\u0003') process.exit();
-  let command = 'Move: ';
-  switch (key) {
-  case 'w':
-    command += 'up';
-    break;
-  case 'a':
-    command += 'left';
-    break;
-  case 's':
-    command += 'down';
-    break;
-  case 'd':
-    command += 'right';
-    break;
-  case 'p':
-    connection.write(`Say: ssssssss`);
-    break;
-  case 'o':
-    connection.write(`Say: SsSssss Ss`);
-    break;
-  case 'i':
-    connection.write(`Say: ssss????`);
-    break;
-  case 'l':
-    connection.write(`Say: SSSSS!`);
-    break;
-  }
-  if (command.length > 6) connection.write(command);
+  if (key === EXIT) process.exit();
+  if (MOVEMENT[key]) return connection.write(`Move: ${MOVEMENT[key]}`);
+  if (CANNED_MESSAGES[key]) return connection.write(`Say: ${CANNED_MESSAGES[key]}`);
 };
 
 // Setup interface to handle user input from stdin
