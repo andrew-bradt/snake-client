6802aef56267f2f1b53bf1fdf096b093860efba0
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-16 14:07:57 -0500

    created constants.js, added IP and port to constants.js, import into client.js

--- client.js
+++ client.js
@@ -1,18 +1,16 @@
 const net = require('net');
+const {IP, PORT} = require('./constants');
 
 const connect = () => {
   const conn = net.createConnection({
-    host: '192.168.0.8',
-    port: '50541'
+    host: IP,
+    port: PORT
   });
-
   conn.setEncoding('utf8');
-
   conn.on('connect', ()=>{
     console.log('Successfully connected to the game server');
     conn.write('Name: @&Y');
   });
-
   return conn;
 };
 
---
+++ constants.js
@@ -0,0 +1,7 @@
+const IP = '192.168.0.8';
+const PORT = '50541';
+
+module.exports = {
+  IP,
+  PORT
+};
